0edd3f5c9e526fd49b51ba44265488a39a32339b
edureka <support@edureka.co>
2023-07-18 16:53:20 +0000

    First commit of HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//This is a simple java program
+//FileName: "HelloWorld.java"
+
+class HelloWorld {
+	// Your program begins with a call to main().
+	// Prints "Hello Edureka" to the terminal windows
+	public static void main(String args[])
+	{
+		System.out.println("Hello Edureka!!!");
+	}
+}
