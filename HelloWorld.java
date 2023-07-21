fe1e78f2b3b7bd7674a0b7fc1f2a48914671b5ae
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2023-07-21 22:16:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld {
 	// Prints "Hello Edureka" to the terminal windows
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Git's Online Training!!");
+		System.out.println("Welcome to Jenkins's Online Training!!");
 	}
 }
